4377103a18011282f1e3b6731e9b28642bd56cd1
Evan McCullough <webmaster@evanamccullough.com>
2015-01-13 23:02:04 -0500

    added thing to fix omniauht

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook, :twitter]
 
 
-  validates :fname, presence: true
-  validates :lname, presence: true
-  validates :birthday, presence: true
-  validates :gender, presence: true
-  validates :hometown_zip, presence: true
-  validates :favorite_sport, presence: true
-  validates :username, presence: true
+  # validates :fname, presence: true
+  # validates :lname, presence: true
+  # validates :birthday, presence: true
+  # validates :gender, presence: true
+  # validates :hometown_zip, presence: true
+  # validates :favorite_sport, presence: true
+  # validates :username, presence: true
 
   before_create :setpoints
 
