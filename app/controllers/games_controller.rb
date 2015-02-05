d85e132b752b15cec2351b3494ac85203b94bc87
Evan McCullough <webmaster@evanamccullough.com>
2015-02-04 22:32:16 -0500

    updated game#index, game#show

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,6 @@
 class GamesController < ApplicationController
   def index
+    # @games = Game.where("date > ?", Time.now).order('date ASC')
     @games = Game.all
   end
 
@@ -8,6 +9,14 @@ class GamesController < ApplicationController
 
   def show
     @game = Game.find(params[:id])
+    @user = User.find_by_id(@game.user_id)
+    @level = Level.where("points > #{@user.points}").first
+
+    now = Time.now.utc.to_date
+    @age = now.year - @user.birthday.year - (@user.birthday.to_date.change(:year => now.year) > now ? 1 : 0)
+
+    @title = "#{Sports.find_by_id(@game.sport_id).name} Game at #{@game.venue} - "
+    @description = "@#{@user.username} created a #{Sports.find_by_id(@game.sport_id).name} Game at #{@game.venue} on #{@game.date.to_formatted_s(:long_ordinal)}"
   end
 
   def new
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,6 @@
 class Game < ActiveRecord::Base
   belongs_to :user
   has_many :sport
+
+  validates :venue, presence: true
 end
